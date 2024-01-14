c5e04868d23e60a723fdd1a423d9ecc6fed84fce
tHyt-lab <washers.fezzes-0q@icloud.com>
2024-01-14 09:55:26 +0000

    Add VPC, SecurityGroup, EC2Instance, and EC2EIP resources

--- lib/ec2eic-stack.ts
+++ lib/ec2eic-stack.ts
@@ -1,16 +1,58 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  CfnInstanceConnectEndpoint,
+  Instance,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  MachineImage,
+  Port,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
 
-export class Ec2EicStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class Ec2EicStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new Vpc(this, "VPC", {
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: "private",
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Ec2EicQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // SecurityGroup
+    const instanceSG = new SecurityGroup(this, "InstanceSG", {
+      vpc,
+    });
+
+    const eicSG = new SecurityGroup(this, "EICSG", {
+      vpc,
+      allowAllOutbound: false,
+    });
+
+    instanceSG.addIngressRule(eicSG, Port.tcp(22));
+    eicSG.addEgressRule(instanceSG, Port.tcp(22));
+
+    // EC2Instance
+    new Instance(this, "Instance", {
+      vpc,
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: MachineImage.latestAmazonLinux2023(),
+      securityGroup: instanceSG,
+    });
+
+    // EC2EIP
+    new CfnInstanceConnectEndpoint(this, "EIP", {
+      subnetId: vpc.selectSubnets({ subnetType: SubnetType.PRIVATE_ISOLATED }).subnetIds[0],
+      securityGroupIds: [eicSG.securityGroupId],
+    });
   }
 }
